6b592a6400f1228dff0a10ddb34ecb8978effba2
JimryYchao <17889982535@163.com>
2024-03-17 09:38:36 +0800

    映射切片 补充

--- _02_Go Library/src/test/test.go
+++ _02_Go Library/src/test/test.go
@@ -2,13 +2,41 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 func main() {
+	s_map := map[string]bool{
+		"a": true,
+		"b": true,
+		"c": true,
+		"d": true,
+		"e": true,
+		"f": true,
+		"h": true,
+		"i": true,
+		"j": true,
+		"k": true,
+		"m": true}
 
-	go func() {
-		fmt.Printf("Hello World")
-	}()
-	time.Sleep(50000)
+	var counter = struct {
+		sync.RWMutex
+		m map[string]bool
+	}{m: s_map}
+
+	keys := make([]string, 0, len(s_map))
+	for k := range s_map {
+		keys = append(keys, k)
+	}
+
+	for _, key := range keys {
+		go func() {
+			counter.RLock()
+			counter.m[key] = false
+			fmt.Println(key, counter.m[key])
+			counter.RUnlock()
+		}()
+	}
+	time.Sleep(10000)
 }
